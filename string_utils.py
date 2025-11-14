125112934d27810059fc4e949901cd41e8093386
tshavit2011-bit <tshavit2011@gmail.com>
2025-11-15 00:33:44 +0200

    Update string_utils.py

--- string_utils.py
+++ string_utils.py
@@ -1,5 +1,19 @@
-def split_before_each_uppercases(formula):
-    pass # Replace the `pass` with your code
+
+ def split_at_first_digit(formula):
+    digit = 1
+
+    for ch in formula[1:]:
+        if ch.isdigit():
+            break
+        else:
+            digit += 1
+
+    if digit == len(formula):
+        return formula, 1
+
+    prefix = formula[:digit]
+    number = int(formula[digit:])
+    return prefix, number    
 
 
 def split_at_first_digit(formula):
